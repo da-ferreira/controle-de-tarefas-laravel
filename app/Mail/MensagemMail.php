7e0b4073a69ca4e53257d21f0f1bfee43db8f5c0
David Ferreira <ferreiradavid361@gmail.com>
2022-06-10 16:04:47 -0300

    feat: configurando mensagem de email para redefinição de senha

---
+++ app/Mail/MensagemMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class MensagemMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Construção da mensagem
+     *
+     * Quando a classe for instanciada, o Laravel passará essa view como renderização dessa classe
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('emails.mensagem');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Mail\MensagemMail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,3 +23,11 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
+
+Route::get('/mensagem-teste', function () {
+    return new MensagemMail();
+
+    // Enviando um email passando a instancia de MensagemMail
+    // Mail::to('jorgearagao8888@gmail.com')->send(new MensagemMail());
+    // return 'Email enviado com sucesso!';
+});
